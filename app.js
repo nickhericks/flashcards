eacc989c2ae56e9f95313cb103c9d54ece8040bc
Nick-Hericks <nickhericks@gmail.com>
2019-04-03 07:58:12 +0200

    Add comments

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
+// When request is made to main url
 app.get('/', (req, res) => {
-	res.render('index');
+	// Assign username cookie to a variable
+	const name = req.cookies.username;
+	// If there is a username cookie
+	if (name) {
+		// Display the index.pug template and pass it the name variable
+		res.render("index", { name });
+	} 
+	// If no cookie
+	else {
+		// Redirect to /hello url
+		res.redirect('/hello');
+	}
 });
 
+// When a GET request is made to the /hello url
 app.get('/hello', (req, res) => {
-	res.render('hello');
+	// Assign username cookie to a variable
+	const name = req.cookies.username;
+	// If there is a username cookie, redirect to main page
+	if (name) {
+		res.redirect('/');
+	} 
+	// If no cookie, display the hello page template
+	else {
+		res.render('hello');
+	}
 });
 
+// When a POST request is made to the /hello url
 app.post('/hello', (req, res) => {
-	res.render('hello', { name: req.body.username });
-	console.dir(req.body);
+	// Create a cookie and send it to the client's browser
+	res.cookie('username', req.body.username);
+	// Redirect user to the main page
+	res.redirect('/');
+	// console.dir(req.body);
 });
 
 app.get('/cards', (req, res) => {
