f4e0c8fb464c9647a4a289b9360bdbec55b21680
Nick-Hericks <nickhericks@gmail.com>
2019-04-04 16:52:52 +0200

    Add button to card.pug

--- routes/cards.js
+++ routes/cards.js
@@ -12,16 +12,29 @@ router.get("/:id", (req, res) => {
 	// If a query string was sent with the request
 	// that includes a 'side' key, assign it to a variable
 	const side = req.query.side;
-	
+	// Access id from the route parameter and assign it a variable
 	const id = req.params.id;
+	// Use the id variable as the index of the card
+	// and the side variable to choose the
+	// property name of 'question' or 'answer'
+	// and assign it to a variable
 	const text = cards[id][side];
+	// Store a reference to the hint as a variable
 	const hint = cards[id].hint;
 
-	const templateData = { text, hint };
-  res.render("card", {
-    prompt: cards[req.params.id].question,
-    hint: cards[req.params.id].hint
-  });
+	const otherSide = (side === 'question') ? 'answer' : 'question';
+	
+	// Put the text variable in an object
+	// that can be passed into the template
+	const templateData = { text, hint, id, side, otherSide };
+
+	// Only display the hint if the 'question' side
+	// is requested, not when 'answer' side is requested
+	// if (side === 'question'){
+	// 	templateData.hint = hint;
+	// }
+
+  res.render('card', templateData);
 });
 
 module.exports = router;
