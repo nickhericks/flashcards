a86165b840ab2ab3011fd47c1fb2fe4dd1c9e136
Nick-Hericks <nickhericks@gmail.com>
2019-04-04 12:09:35 +0200

    Check for query string

--- routes/cards.js
+++ routes/cards.js
@@ -9,6 +9,8 @@ const { cards } = data;
 // in the app.js file we are using it as '/cards'
 // the code below can just refer to the route as '/'
 router.get("/:id", (req, res) => {
+	const { side } = req.query;
+
   res.render("card", {
     prompt: cards[req.params.id].question,
     hint: cards[req.params.id].hint
